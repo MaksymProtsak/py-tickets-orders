e6208375539816912c4f3be3afbb15ea92a63b86
Maksym Protsak <maksym.protsak@gmail.com>
2024-11-10 22:55:39 +0200

    Added tickets_available in MovieSessionListSerializer field, in Meta too. Created TakenPlacesSerializer. Added taken_places field in MovieSessionDetailSerializer, updated Meta.

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -68,6 +68,7 @@ class MovieSessionListSerializer(serializers.ModelSerializer):
     cinema_hall_capacity = serializers.IntegerField(
         source="cinema_hall.capacity", read_only=True
     )
+    tickets_available = serializers.IntegerField(read_only=True)
 
     class Meta:
         model = MovieSession
@@ -77,16 +78,28 @@ class MovieSessionListSerializer(serializers.ModelSerializer):
             "movie_title",
             "cinema_hall_name",
             "cinema_hall_capacity",
+            "tickets_available",
         )
+
+
+class TakenPlacesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ticket
+        fields = ("row", "seat",)
 
 
 class MovieSessionDetailSerializer(MovieSessionSerializer):
     movie = MovieListSerializer(many=False, read_only=True)
     cinema_hall = CinemaHallSerializer(many=False, read_only=True)
+    taken_places = TakenPlacesSerializer(
+        many=True,
+        read_only=True,
+        source="tickets"
+    )
 
     class Meta:
         model = MovieSession
-        fields = ("id", "show_time", "movie", "cinema_hall")
+        fields = ("id", "show_time", "movie", "cinema_hall", "taken_places")
 
 
 class TicketSerializer(serializers.ModelSerializer):
@@ -140,4 +153,3 @@ class OrderSerializerList(serializers.ModelSerializer):
     class Meta:
         model = Order
         fields = ("id", "tickets", "created_at",)
-
